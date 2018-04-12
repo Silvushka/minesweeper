7bff89b77ab87abecc1424e57cf814d55097a94a
P3502858 <P3502858@C3505100361.ness.com>
2018-04-12 15:11:27 +0200

    task 5

---
+++ test/minesweeper/core/FieldTest.java
@@ -0,0 +1,78 @@
+package minesweeper.core;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class FieldTest {
+
+	static final int ROWS = 9;
+	static final int COLUMNS = 9;
+	static final int MINES = 10;
+
+	@Test
+	public void isSolved() {
+		Field field = new Field(ROWS, COLUMNS, MINES);
+
+		assertEquals(GameState.PLAYING, field.getState());
+
+		int open = 0;
+		for (int row = 0; row < field.getRowCount(); row++) {
+			for (int column = 0; column < field.getColumnCount(); column++) {
+				if (field.getTile(row, column) instanceof Clue) {
+					field.openTile(row, column);
+					open++;
+				}
+				if (field.getRowCount() * field.getColumnCount() - open == field.getMineCount()) {
+					assertEquals(GameState.SOLVED, field.getState());
+				} else {
+					assertNotSame(GameState.FAILED, field.getState());
+				}
+			}
+		}
+
+		assertEquals(GameState.SOLVED, field.getState());
+	}
+
+	@Test
+	public void generate() {
+		Field field = new Field(ROWS, COLUMNS, MINES);
+		
+		int mineCount = 0;
+		int clueCount = 0;
+		
+		assertEquals(ROWS, field.getRowCount());
+		assertEquals(COLUMNS, field.getColumnCount());
+		assertEquals(MINES, field.getMineCount());
+
+		for (int row = 0; row < field.getRowCount(); row++) {
+			for (int column = 0; column < field.getColumnCount(); column++) {
+				assertNotNull(field.getTile(row, column));
+			}
+		}
+
+		for (int row = 0; row < field.getRowCount(); row++) {
+			for (int column = 0; column < field.getColumnCount(); column++) {
+				if (field.getTile(row, column) instanceof Mine) {
+					mineCount++;
+				}
+			}
+		}
+
+		assertEquals(MINES, mineCount);
+		
+		for (int row = 0; row < field.getRowCount(); row++) {
+			for (int column = 0; column < field.getColumnCount(); column++) {
+				if (field.getTile(row, column) instanceof Clue) {
+					clueCount++;
+				}
+			}
+		}
+
+		assertEquals(ROWS * COLUMNS - MINES, clueCount);
+
+	}
+
+}
